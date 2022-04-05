429ab3e1faf3ddaadfe9ccf25418f4061148fb20
Tino Reyna <tinoreyna1984@gmail.com>
2022-04-05 16:56:54 -0500

    metodos crud

--- contacto/models.py
+++ contacto/models.py
@@ -7,3 +7,6 @@ class Contacto(models.Model):
     fono = models.CharField(max_length=200)
     email = models.CharField(max_length=200, blank=True, null=True)
     fecha_nac = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
