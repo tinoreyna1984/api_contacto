0389afdcbec55e54dab708bc53d3361117cfe669
Tino Reyna <tinoreyna1984@gmail.com>
2022-04-05 15:18:43 -0500

    metodos crud

--- contacto/views.py
+++ contacto/views.py
@@ -7,15 +7,6 @@ from .serializers import ContactoSerializer
 # Create your views here.
 
 class ContactoViewSet(viewsets.ModelViewSet):
-    perms_map = {
-        'GET': [],
-        'OPTIONS': [],
-        'HEAD': [],
-        'POST': [],
-        'PUT': [],
-        'PATCH': [],
-        'DELETE': [],
-    }
     permission_classes = [AllowAny]
     serializer_class = ContactoSerializer
     queryset = Contacto.objects.all()
