3d739f7df2c5d1dda3e13c58212943a10a6c903b
Tino Reyna <tinoreyna1984@gmail.com>
2022-04-04 23:09:01 -0500

    Correcciones

--- api_contacto/settings.py
+++ api_contacto/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
