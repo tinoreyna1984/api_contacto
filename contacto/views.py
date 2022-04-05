8728655bac38d60790d8823464dcffaea21308de
Tino Reyna <tinoreyna1984@gmail.com>
2022-04-05 16:01:42 -0500

    metodos crud

--- contacto/views.py
+++ contacto/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework import viewsets
+from rest_framework import status
 from .models import Contacto
 from .serializers import ContactoSerializer
 
@@ -11,3 +13,23 @@ class ContactoViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
     serializer_class = ContactoSerializer
     queryset = Contacto.objects.all()
+
+    def retrieve(self, request, pk=None):
+        """Handles getting an object by its ID."""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handles updating part of an object."""
+
+        return Response({'http_method': 'PATCH'})
+    
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+
+        return Response({'http_method': 'DELETE'})
