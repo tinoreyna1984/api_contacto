c036b4c8294f562c1adde650e7b0edfaf3cae863
Tino Reyna <tinoreyna1984@gmail.com>
2022-04-05 15:22:21 -0500

    metodos crud

--- contacto/views.py
+++ contacto/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
 from rest_framework import viewsets
 from .models import Contacto
 from .serializers import ContactoSerializer
@@ -6,5 +7,15 @@ from .serializers import ContactoSerializer
 # Create your views here.
 
 class ContactoViewSet(viewsets.ModelViewSet):
+    perms_map = {
+        'GET': [],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': [],
+        'PUT': [],
+        'PATCH': [],
+        'DELETE': [],
+    }
+    permission_classes = [AllowAny]
     serializer_class = ContactoSerializer
     queryset = Contacto.objects.all()
