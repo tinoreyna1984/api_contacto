7372d584eeaef128e29e7f2030a1856096cfe910
Tino Reyna <tinoreyna1984@gmail.com>
2022-04-04 23:35:30 -0500

    correcciones

--- api_contacto/settings.py
+++ api_contacto/settings.py
@@ -129,4 +129,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
