191f00a6c9c2c76ac288010bc6af42d166132c0e
Tino Reyna <tinoreyna1984@gmail.com>
2022-04-04 23:04:25 -0500

    DEBUG=False

--- api_contacto/settings.py
+++ api_contacto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q!p$@qob=t=2=m&1sws%)^qhg@r_n0iaj_hu#1y3bwp^9*omcc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
