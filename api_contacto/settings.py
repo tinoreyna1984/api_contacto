54737cb7598fd898065806e06b9618b9b591f0be
Tino Reyna <tinoreyna1984@gmail.com>
2022-04-05 21:41:38 -0500

    corrige declaracion

--- api_contacto/settings.py
+++ api_contacto/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware", # agregar
+    'corsheaders.middleware.CorsMiddleware', # agregar
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
